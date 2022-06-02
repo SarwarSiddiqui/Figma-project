ad09682482314e78731d30270e5018f7369985d4
SarwarSiddiqui <SarwarSiddiqui721@gmail.com>
2022-06-02 16:08:53 +0530

    we fixed the component portion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import HeaderComponent from "./component/Header-section/header.js";
-import Header from "./component/Header-section/Header.css";
 
 function App() {
   return (
     <div>
       <HeaderComponent />
-      <Header />
     </div>
   );
 }
--- src/component/Header-section/header.js
+++ src/component/Header-section/header.js
@@ -1,16 +1,16 @@
 import "./Header.css";
-import Icon from "./Images/Icon.png";
+import Icon from "../../Images/Icon.png";
 
 function HeaderComponent() {
   return (
-    <div class="Head-section">
-      <div class="ch">
-        <img src={Icon} alt="Pen Icon" class="Icon" />
-        <h4 class="Head-text1">Edit page</h4>
+    <div className="head-section">
+      <div className="head-section-edit-icon">
+        <img src={Icon} alt="Pen Icon" className="head-section-icon" />
+        <h4 className="head-section-edit-text">Edit page</h4>
       </div>
-      <h4 class="Head-text2">Hello Admin!</h4>
+      <h4 className="head-section-editing">Hello Admin!</h4>
     </div>
   );
 }
 
-export default HeaderComponent();
+export default HeaderComponent;
