b685cf868caddca5645d1d59350bf3b32d3e91e9
SarwarSiddiqui <SarwarSiddiqui721@gmail.com>
2022-06-15 00:07:49 +0530

    The ellipse image is not fixed under the shape image

--- src/component/Navbar-section/nav.js
+++ src/component/Navbar-section/nav.js
@@ -7,14 +7,29 @@ import AppFrame from "../../Images/AppFrame.jpg";
 
 export default function Navbar() {
   return (
-    <div id="container">
-      <img src={AppFrame} alt="Frame-image" className="Nav-frame" />
-      ;
-      <img src={Shape} alt="bell-shape" className="Nav-Shape" />
-      ;
-      <img src={Iconbutton} alt="star-shape" className="Nav-button" />
-      ;
-      <img src={IconAccount} alt="Account-icon" className="Nav-Account" />;
+    <div className="main-container">
+      <div id="container">
+        <img src={AppFrame} alt="Frame-image" className="Nav-frame" />
+        <img src={Shape} alt="bell-shape" className="Nav-Shape" />
+        <img src={Iconbutton} alt="star-shape" className="Nav-button" />
+        <img src={IconAccount} alt="Account-icon" className="Nav-Account" />
+      </div>
+      <div className="Nav-bell">
+        <img src={Ellipse} alt="bell-shape-ellipse" className="Nav-ellipse" />
+      </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
